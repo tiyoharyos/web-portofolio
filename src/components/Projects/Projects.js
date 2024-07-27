f09f1ab5e2597df61663b53757da87fed2f29011
Tiyo Haryo Subaktiono <87368516+tiyoharyos@users.noreply.github.com>
2024-07-27 15:50:50 +0700

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 import iot from "../../Assets/Projects/iot.png";
 import nyolo from "../../Assets/Projects/nyolo.png";
 
